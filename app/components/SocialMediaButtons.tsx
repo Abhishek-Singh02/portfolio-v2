4f6e3853eab75b776166053e74b68ed31c3120cd
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-08 23:46:19 +0530

    refactor: create Social Media Links component

---
+++ app/components/SocialMediaButtons.tsx
@@ -0,0 +1,49 @@
+import { SocialMediaType } from "@/sanity";
+import parse from "html-react-parser";
+import { cloneElement, FC } from "react";
+
+export type SocialMediaLinksProps = {
+  socialMedias: SocialMediaType[];
+};
+
+export const SocialMediaLinks: FC<SocialMediaLinksProps> = ({
+  socialMedias,
+}) => {
+  return (
+    <div className="flex gap-4 mt-8">
+      {socialMedias.map((sm, index) => (
+        <SocialMediaLink key={index} socialMedia={sm} />
+      ))}
+    </div>
+  );
+};
+
+export type SocialMediaLinkProps = {
+  socialMedia: SocialMediaType;
+};
+
+export const SocialMediaLink: FC<SocialMediaLinkProps> = ({ socialMedia }) => {
+  const { url, icon } = socialMedia;
+  const component: any = parse(icon, {
+    replace(ele) {
+      if (ele.type === "script") return <></>;
+    },
+  });
+  const ICON_SIZE = "28";
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className="block hover:text-slate-200"
+    >
+      <span className="w-8 h-8">
+        {cloneElement(component, {
+          width: ICON_SIZE,
+          height: ICON_SIZE,
+          strokeWidth: "1.5",
+        })}
+      </span>
+    </a>
+  );
+};
