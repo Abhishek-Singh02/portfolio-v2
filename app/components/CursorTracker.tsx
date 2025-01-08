241cbc4f8524b644e7fc5c989c54a038c5df3031
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-08 23:45:27 +0530

    refactor: create Cursor Tracker component

---
+++ app/components/CursorTracker.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { FC, useEffect, useState } from "react";
+
+export type CursorTrackerProps = {};
+
+export const CursorTracker: FC<CursorTrackerProps> = () => {
+  const [pos, setPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (ev: any) => {
+      setPos({ x: ev.clientX, y: ev.clientY + window.scrollY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+  return (
+    <div
+      className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+      style={{
+        background: `radial-gradient(600px at ${pos.x}px ${pos.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+      }}
+    ></div>
+  );
+};
