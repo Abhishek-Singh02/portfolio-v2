31b1e7d1bd134be4dcd13e2fcb7c8216b696cf04
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-09 18:18:27 +0530

    refactor: add ability to parse text in about section

--- app/components/sections/AboutSection.tsx
+++ app/components/sections/AboutSection.tsx
@@ -1,6 +1,7 @@
 import { AboutType } from "@/sanity";
 import { FC } from "react";
 import { BaseSection } from "./BaseSection";
+import parse, { attributesToProps, domToReact } from "html-react-parser";
 
 export type AboutSectionProps = {
   about: AboutType;
@@ -10,11 +11,34 @@ export const AboutSection: FC<AboutSectionProps> = ({ about }) => {
   return (
     <BaseSection id="about" title="About">
       <div>
-        {about.sections.map((section, index) => (
-          <p className="mb-4" key={index}>
-            {section}
-          </p>
-        ))}
+        {about.sections.map((section, index) => {
+          const options = {
+            replace(ele: any) {
+              if (ele.type === "script") {
+                return <></>;
+              } else if (ele.type === "tag" && ele.name === "a") {
+                const props = attributesToProps(ele.attribs);
+                return (
+                  <a
+                    target="_blank"
+                    referrerPolicy="no-referrer"
+                    {...props}
+                    className="text-teal-400"
+                  >
+                    {domToReact(ele.children, options)}
+                  </a>
+                );
+              }
+            },
+          };
+          const component: any = parse(section, options);
+
+          return (
+            <p className="mb-4" key={index}>
+              {component}
+            </p>
+          );
+        })}
       </div>
     </BaseSection>
   );
