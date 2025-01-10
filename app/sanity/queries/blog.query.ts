09807fa0ff5c5dd5bfc3a7588a8487968d68ae64
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-10 16:10:16 +0530

    refactor: select order as well in blogs and projects query

--- app/sanity/queries/blog.query.ts
+++ app/sanity/queries/blog.query.ts
@@ -1,3 +1,3 @@
 import groq from "groq";
 
-export const BLOG_QUERY = groq`*[_type == "blog"]{name,year,madeAt,description,tags,url,"thumbnail": thumbnail.asset->url,showOnHome} | order(order asc)`;
+export const BLOG_QUERY = groq`*[_type == "blog"]{name,year,madeAt,description,tags,url,"thumbnail": thumbnail.asset->url,showOnHome,order} | order(order asc)`;
