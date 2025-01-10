8d4172efbf37e738d467a78dc1505af20dff05b9
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-10 16:03:52 +0530

    refactor: fix list style and shorten url in archive projects list

--- app/routes/archive.tsx
+++ app/routes/archive.tsx
@@ -47,7 +47,7 @@ export default function Index() {
           >
             <thead className="sticky top-0 z-10 border-b border-slate-300/10 bg-slate-900/75 px-6 py-5 backdrop-blur">
               <tr>
-                <th className="py-4 text-sm font-semibold text-slate-200">
+                <th className="py-4 pr-4 text-sm font-semibold text-slate-200">
                   Year
                 </th>
                 <th className="py-4 pr-8 text-sm font-semibold text-slate-200">
@@ -70,7 +70,7 @@ export default function Index() {
                   className="border-b border-slate-300/10 last:border-none"
                   key={`${project.name}_${index}`}
                 >
-                  <td className="py-4 align-top text-sm">
+                  <td className="py-4 pr-4 align-top text-sm">
                     <div className="translate-y-px">{project.year}</div>
                   </td>
                   <td className="py-4 pr-4 align-top font-semibold leading-snug text-slate-200">
@@ -125,32 +125,34 @@ export default function Index() {
                   <td className="hidden py-4 align-top sm:table-cell">
                     <ul className="translate-y-1">
                       <li className="mb-1 flex items-center">
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 text-sm text-slate-400 hover:text-slate-200 focus-visible:text-teal-300 group/link text-sm"
-                          href={project.url}
-                          target="_blank"
-                          rel="noreferrer noopener"
-                          aria-label={`${project.name} (opens in a new tab)`}
-                        >
-                          <span>
-                            <span className="inline-block">
-                              {project.url}
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                  clipRule="evenodd"
-                                ></path>
-                              </svg>
+                        {project?.url?.length && (
+                          <a
+                            className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 text-sm text-slate-400 hover:text-slate-200 focus-visible:text-teal-300 group/link text-sm"
+                            href={project.url}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            aria-label={`${project.name} (opens in a new tab)`}
+                          >
+                            <span>
+                              <span className="inline-block">
+                                {project.url.split("/").slice(0, 3).join("/")}
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                    clipRule="evenodd"
+                                  ></path>
+                                </svg>
+                              </span>
                             </span>
-                          </span>
-                        </a>
+                          </a>
+                        )}
                       </li>
                     </ul>
                   </td>
