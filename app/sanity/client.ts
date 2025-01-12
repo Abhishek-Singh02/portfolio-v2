d7ed86c676c0afd5dd150f179c2f19fa35d89545
abhishek9502as@gmail.com <abhishek9502as@gmail.com>
2025-01-12 13:31:56 +0530

    refactor: remove stega config from sanity client

--- app/sanity/client.ts
+++ app/sanity/client.ts
@@ -1,13 +1,9 @@
 import { createClient } from "@sanity/client";
-import { stegaEnabled, projectId, dataset, studioUrl } from "./projectDetails";
+import { dataset, projectId } from "./projectDetails";
 
 export const client = createClient({
   projectId,
   dataset,
   useCdn: true,
   apiVersion: "2023-03-20",
-  stega: {
-    enabled: stegaEnabled,
-    studioUrl,
-  },
 });
